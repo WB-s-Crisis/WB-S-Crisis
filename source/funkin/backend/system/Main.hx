3e488ab3f6a43da63271e90d28e61dc56402422a
GeXie19 <3449326235@qq.com>
2025-01-30 21:05:43 +0800

    Update Main.hx

--- source/funkin/backend/system/Main.hx
+++ source/funkin/backend/system/Main.hx
@@ -2,7 +2,6 @@ package funkin.backend.system;
 
 import funkin.editors.SaveWarning;
 import funkin.backend.assets.AssetsLibraryList;
-import funkin.backend.system.framerate.SystemInfo;
 import openfl.utils.AssetLibrary;
 import openfl.text.TextFormat;
 import flixel.system.ui.FlxSoundTray;
