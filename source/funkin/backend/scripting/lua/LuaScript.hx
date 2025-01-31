7e71c7a87558efa341e14c2e4facf26c860f883a
GeXie19 <3449326235@qq.com>
2025-01-31 14:37:58 +0800

    Create LuaScript.hx

---
+++ source/funkin/backend/scripting/lua/LuaScript.hx
@@ -0,0 +1,49 @@
+package funkin.backend.scripting.lua;
+
+import haxe.io.Path;
+
+import llua.*;
+import llua.Lua;
+
+class LuaScript extends FlxBasic {
+  public var state:State = null;
+  
+  public var fileName:String;
+  public var path:String;
+  
+  public function new(path:String) {
+    this.path = path;
+    fileName = Path.withoutDirectory(path);
+
+    onCreate(path);
+
+    super();
+  }
+
+  public function onCreate(path:String) {
+    state = LuaL.newstate();
+    LuaL.openlibs(state);
+
+    switch(Path.extension(path)) {
+        case "lua":
+          if(Assets.exists(path)) {
+            Lua.dostring(state, Assets.getText(path));
+          }else close();
+        default: close();
+    }
+
+    Lua_helper.add_callback(state, debugPrint, function(text:String, ?delayTime:Float = 1) {
+      Main.game.debugPrint(text, delayTime);
+    });
+    Lua.getGlobal(state, "test");
+    Lua.pcall(lua, 3, 0, 1);
+
+    close();
+  }
+
+  public function close():Void {
+    if(state == null) return;
+    
+    Lua.close(state);
+  }
+}
