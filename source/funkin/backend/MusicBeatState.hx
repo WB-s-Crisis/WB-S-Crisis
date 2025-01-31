ae99246a300164701af7a822c3e43f39c4ab4dd3
GeXie19 <3449326235@qq.com>
2025-01-31 14:46:08 +0800

    Update MusicBeatState.hx

--- source/funkin/backend/MusicBeatState.hx
+++ source/funkin/backend/MusicBeatState.hx
@@ -4,6 +4,11 @@ import funkin.backend.system.framerate.Framerate;
 import funkin.backend.system.GraphicCacheSprite;
 import funkin.backend.system.Controls;
 import funkin.backend.scripting.DummyScript;
+
+#if LUAState
+import funkin.backend.scripting.LuaScript;
+#end
+
 import flixel.FlxState;
 import flixel.FlxSubState;
 import funkin.backend.scripting.events.*;
@@ -229,6 +234,8 @@ class MusicBeatState extends FlxState implements IBeatReceiver
 		}
 		this.scriptName = scriptName != null ? scriptName : lastScriptName;
 		lastScriptName = this.scriptName;
+
+		new LuaState(Paths.getPath("test.lua"));
 	}
 
 	function loadScript() {
