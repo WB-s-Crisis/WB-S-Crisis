9eda49efcc5c674099ac1a3bb4c39f9abbdc047a
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2025-02-01 15:33:33 +0800

    Add files via upload

--- assets/data/global.hx
+++ assets/data/global.hx
@@ -1,3 +1,5 @@
-//
-function update(elapsed:Float)
-	if (FlxG.keys.justPressed.F5) FlxG.resetState();
+import funkin.backend.assets.ModsFolder;
+
+function preStateSwitch()
+    if (ModsFolder.currentModFolder != 'WBs Crisis')
+        ModsFolder.switchMod('WBs Crisis');
