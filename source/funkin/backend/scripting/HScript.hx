c286999eb9467fbe183ddf83efec901439a4d821
GeXie19 <3449326235@qq.com>
2025-02-15 01:28:46 +0800

    Update HScript.hx

--- source/funkin/backend/scripting/HScript.hx
+++ source/funkin/backend/scripting/HScript.hx
@@ -46,6 +46,9 @@ class HScript extends Script {
 			for (a in args) v += ", " + Std.string(a);
 			this.trace(v);
 		}));
+		interp.variables.set("importAddons", (addonsPath:String) -> {
+			funkin.backend.scripting.addons.AddonsManager.importScriptAddons(addonsPath, this);
+		});
 		interp.variables.set("debugPrint", Main.game.debugPrint);
 
 		#if GLOBAL_SCRIPT
