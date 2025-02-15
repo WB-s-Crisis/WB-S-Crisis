28f6599b1a3e070dfa7d3850abed16ea165ccb44
GeXie19 <3449326235@qq.com>
2025-02-15 15:32:29 +0800

    Add files via upload

--- source/funkin/extra/UnusedVideoState.hx
+++ source/funkin/extra/UnusedVideoState.hx
@@ -4,24 +4,56 @@ import flixel.FlxState;
 import hxvlc.openfl.Video;
 import hxvlc.flixel.FlxVideoSprite;
 import flixel.util.FlxTimer;
+import flixel.util.FlxSave;
+import flixel.text.FlxText;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxAxes;
 
 /**
  * 废物东西
  */
 class UnusedVideoState extends FlxState {
+	public static var canSkip(default, set):Bool = false;
+	@:noCompletion private static function set_canSkip(val:Bool):Bool {
+		if(save != null) {
+			Reflect.setField(save.data, "canSkip", val);
+			save.flush();
+		}
+		
+		return val;
+	}
+	
+	public static var save:FlxSave = null;
+	public static function init() {
+		if(save == null) {
+			save = new FlxSave();
+			save.bind("unusedVideo");
+			if(Reflect.hasField(save.data, "canSkip")) {
+				canSkip = save.data.canSkip;
+			}
+		}
+	}
+
+	public var skipText:FlxText;
+
 	public var video:FlxVideoSprite;
 	var started:Bool = false;
 	var finished:Bool = false;
+	
+	private var realCanSkip:Bool = false;
 	
 	var startDelay:Float = 0.001;
 
 	var path:String;
 	var callbackOptions:CallbackOptions;
+	
+	var nextState:FlxState;
 
-	public function new(path:String, ?startDelay:Float = 0.001, ?callbackOptions:CallbackOptions) {
+	public function new(path:String, nextState:FlxState, ?startDelay:Float = 0.001, ?callbackOptions:CallbackOptions) {
 		super();
 		
 		this.path = path;
+		this.nextState = nextState;
 		if(callbackOptions != null)
 			this.callbackOptions = callbackOptions;
 		
@@ -38,18 +70,18 @@ class UnusedVideoState extends FlxState {
 				video.updateHitbox();
 			}
 		});
-		video.bitmap.onEndReached.add(() -> {
-			finished = true;
-			video.destroy();
-			if(Reflect.hasField(this.callbackOptions, "onFinish") && this.callbackOptions.onFinish != null) {
-				this.callbackOptions.onFinish(video);
-			}
-		});
+		video.bitmap.onEndReached.add(finish);
 		video.bitmap.onPlaying.add(() -> {
 			started = true;
 			if(Reflect.hasField(this.callbackOptions, "onStart") && this.callbackOptions.onStart != null) {
-				this.callbackOptions.onStart(video);
+				this.callbackOptions.onStart(this);
 			}
+			
+			if(canSkip) {
+				FlxTween.tween(skipText, {alpha: 1}, 0.25, {startDelay: 2.5, onComplete: function(tween:FlxTween) {
+					realCanSkip = true;
+				}});
+			}else canSkip = true;
 		});
 	
 		if(video.load(this.path))
@@ -58,6 +90,15 @@ class UnusedVideoState extends FlxState {
 			});
 	
 		add(video);
+		
+		skipText = new FlxText(0, 0, FlxG.width, "臭人机", 32);
+		skipText.setFormat(Paths.font("vcr.ttf"), 32, 0xFFFFFFFF, CENTER, OUTLINE, 0xFF6A0000);
+		//skipText.screenCenter(FlxAxes.X);
+		skipText.y = 325;
+		skipText.borderSize = 4;
+		skipText.alpha = 0;
+		skipText.text = #if TOUCH_CONTROLS 'Touch Screen To Skip' #else 'Press Enter To Skip' #end;
+		add(skipText);
 	
 		super.create();
 	}
@@ -67,14 +108,57 @@ class UnusedVideoState extends FlxState {
 		
 		if(started && !finished) {
 			if(Reflect.hasField(this.callbackOptions, "onUpdate") && this.callbackOptions.onUpdate != null) {
-				this.callbackOptions.onUpdate(video, elapsed);
+				this.callbackOptions.onUpdate(this, elapsed);
 			}
+			
+			if(realCanSkip) {
+				#if TOUCH_CONTROLS
+				for(touch in FlxG.touches.list) {
+					if(touch.justPressed) {
+						finish();
+						realCanSkip = false;
+					}
+				}
+				#else
+				if(FlxG.keys.justPressed.ENTER) {
+					finish();
+					realCanSkip = false;
+				}
+				#end
+			}
+		}
+	}
+	
+	private function finish():Void {
+		finished = true;
+
+		if(Reflect.hasField(this.callbackOptions, "onFinish") && this.callbackOptions.onFinish != null) {
+			this.callbackOptions.onFinish(this);
+		}
+		
+		if(skipText.alpha != 0)
+			for(sb in [skipText, video]) {
+				FlxTween.tween(sb, {alpha: 0}, 0.75, {onComplete: function(tween:FlxTween) {
+					new FlxTimer().start(1.2, function(tmr:FlxTimer) {
+						FlxG.switchState(nextState);
+					});
+					video.destroy();
+				}});
+			}
+			FlxTween.num(100, 0, 0.75, function(val:Float) {
+				video.bitmap.volume = Math.floor(val);
+			});
+		else {
+			new FlxTimer().start(1.2, function(tmr:FlxTimer) {
+				FlxG.switchState(nextState);
+			});
+			video.destroy();
 		}
 	}
 }
 
 typedef CallbackOptions = {
-	var ?onFinish:FlxVideoSprite->Void;
-	var ?onStart:FlxVideoSprite->Void;
-	var ?onUpdate:FlxVideoSprite->Float->Void;
+	var ?onFinish:UnusedVideoState->Void;
+	var ?onStart:UnusedVideoState->Void;
+	var ?onUpdate:UnusedVideoState->Float->Void;
 }
