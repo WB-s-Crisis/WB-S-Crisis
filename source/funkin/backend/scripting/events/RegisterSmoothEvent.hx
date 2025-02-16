b9f5f055558cc0085c83b5a77dd17c9001cc23c3
GeXie19 <3449326235@qq.com>
2025-02-16 15:34:52 +0800

    Update RegisterSmoothEvent.hx

--- source/funkin/backend/scripting/events/RegisterSmoothEvent.hx
+++ source/funkin/backend/scripting/events/RegisterSmoothEvent.hx
@@ -6,4 +6,9 @@ final class RegisterSmoothEvent extends CancellableEvent {
   public var smoothTransition:PlayStateTransitionData;
   public var skipTransIn:Bool;
   public var skipTransOut:Bool;
+
+  @:dox(hide) public var skipTransCancelled:Bool = false;
+  @:dox(hide) public function cancelSkipTrans():Void {
+    skipTransCancelled = true;
+  }
 }
