5bfe5ed6155d4b2c39bd6525cf99661f59f3f656
GeXie19 <3449326235@qq.com>
2025-02-25 15:36:36 +0800

    Update CryptoUtil.hx

--- source/funkin/backend/utils/CryptoUtil.hx
+++ source/funkin/backend/utils/CryptoUtil.hx
@@ -1,7 +1,37 @@
 package funkin.backend.utils;
 
-import haxe.crypto.*;
+#if ALLOW_CRYPTO
+import com.hurlant.util.*;
+#end
 
 class CryptoUtil {
-	//nope
+	//暂时只进行base64加密
+	public inline static function convertByteToBase64(beep:haxe.io.Bytes):String {
+		#if ALLOW_CRYPTO
+		return Base64.encodeByteArray(beep);
+		#end
+		return null;
+	}
+
+	//openfl和hurlant都玩的太抽象了，只能这么干了
+	public inline static function convertBase64ToByte(c:String):haxe.io.Bytes {
+		#if ALLOW_CRYPTO
+		return Base64.decodeToByteArray(c);
+		#end
+		return null;
+	}
+
+	public inline static function convertStringToBase64(c:String):String {
+		#if ALLOW_CRYPTO
+		return Base64.encode(c);
+		#end
+		return null;
+	}
+
+	public inline static function convertBase64ToString(c:String):String {
+		#if ALLOW_CRYPTO
+		return Base64.decode(c);
+		#end
+		return null;
+	}
 }
