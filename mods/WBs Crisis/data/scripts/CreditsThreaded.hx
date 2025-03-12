37819dc0bcbcc757ce5bf6443ea4a2a9f6a9a04f
GeXie <3449326235@qq.com>
2025-03-12 21:24:37 +0800

    我被多线程奴隶了

---
+++ mods/WBs Crisis/data/scripts/CreditsThreaded.hx
@@ -0,0 +1,40 @@
+function onLoad(length:Int) {
+	var i = 0;
+	while(i < length) {
+		var sb = parseCreditsFromDirectory(optionStuffixList[i]);
+		
+		//huh？加载失败？直接给我回炉重造！！！
+		if(sb == null) continue;
+		
+		stuffixData.push(sb);
+
+		var dir = optionStuffixList[i];
+
+		if(Assets.exists(imagePath("stuffix/" + dir + "/icon"))) {
+			var bd = Assets.getBitmapData(imagePath("stuffix/" + dir + "/icon"));
+			
+			if(bd == null) continue;
+		
+			if(!Reflect.hasField(stuffixData[i], "color") || stuffixData[i].color == null) {
+				var newColor = BitmapUtil.getMostPresentSaturatedColor(bd);
+				Reflect.setField(stuffixData[i], "color", newColor);
+			}
+		
+			if(Reflect.hasField(stuffixData[i], "maskIcon") && stuffixData[i].maskIcon) {
+				var maskBd = new BitmapData(bd.width, bd.height, true, 0x00000000);
+				var shape = new Shape();
+				shape.graphics.beginFill(0xFF00FF00);
+				shape.graphics.drawCircle(maskBd.width / 2, maskBd.height / 2, ((maskBd.width + maskBd.height) / 2) / 2);
+				shape.graphics.endFill();
+				maskBd.draw(shape);
+			
+				bd.copyChannel(maskBd, new Rectangle(0, 0, bd.width, bd.height), new Point(), 8, 8);
+			}
+		
+			graphicCache.cacheGraphic(FlxG.bitmap.add(bd));
+		}
+	
+		i++;
+		loadedAmout++;
+	}
+}
