d3c75b7e70a5e47dac973006aba9ea5978bc7a6a
GeXie <3449326235@qq.com>
2025-03-13 02:18:43 +0800

    Update MobileUtil.hx

--- source/mobile/funkin/backend/utils/MobileUtil.hx
+++ source/mobile/funkin/backend/utils/MobileUtil.hx
@@ -81,7 +81,7 @@ class MobileUtil
 		}
 
 		if ((VERSION.SDK_INT >= VERSION_CODES.TIRAMISU && !Permissions.getGrantedPermissions().contains('android.permission.READ_MEDIA_IMAGES')) || (VERSION.SDK_INT < VERSION_CODES.TIRAMISU && !Permissions.getGrantedPermissions().contains('android.permission.READ_EXTERNAL_STORAGE')))
-			lime.app.Application.current.window.alert('如果你管意将你手机的文件存储管理权限给我，你将会体验到这个模组飞一般的感觉（对的）\n如果不同意...建议还是同意，省你几步流程，别让我给你解析一份小报告给您\n就说按不按吧！\n(If you accepted the permissions you are all good!' + ')\n(If you didn\'t then expect a crash)' + '\n(Press Ok to see what happens)', '注意！(Notice!)');
+			lime.app.Application.current.window.alert('如果你愿意将你手机的文件存储管理权限给我，你将会体验到这个模组飞一般的感觉（对的）\n如果不同意...建议还是同意，省你几步流程，别让我给你解析一份小报告给您\n就说按不按吧！\n(If you accepted the permissions you are all good!' + ')\n(If you didn\'t then expect a crash)' + '\n(Press Ok to see what happens)', '注意！(Notice!)');
 
 		try
 		{
