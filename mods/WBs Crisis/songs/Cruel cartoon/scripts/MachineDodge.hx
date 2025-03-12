7c2c73ed91ff1ea613c9f868dfac99531326d4b0
GeXie <3449326235@qq.com>
2025-03-12 13:59:33 +0800

    上传curel cartoon脚本

---
+++ mods/WBs Crisis/songs/Cruel cartoon/scripts/MachineDodge.hx
@@ -0,0 +1,132 @@
+importAddons("game.AttackSystem");
+
+var dodgeSystem:AttackSystem;
+var characterLockCurAnim:Array<Bool> = [];
+var attackSound:FlxSound = FlxG.sound.load(Paths.sound("game/attack"));
+var attackNoDodgeSound:FlxSound = FlxG.sound.load(Paths.sound("game/blood"));
+
+var drainCount:Int = 8;
+
+function new() {
+	for(i in 0...strumLines.length) {
+		characterLockCurAnim.push(false);
+	}
+}
+
+function onPlayerHit(event) {
+	event.healthGain = 0.023 * (drainCount / 8);
+}
+
+function create() {
+	dodgeSystem = new AttackSystem((obj) -> {
+		obj.frames = Paths.getSparrowAtlas("game/spacebar");
+		obj.animation.addByPrefix("static", "static", 1, true);
+		obj.animation.addByPrefix("press", "press", 1, true);
+		obj.animation.play("static", true);
+		obj.scale.set(0.6, 0.6);
+		obj.updateHitbox();
+		obj.alpha = 0;
+		obj.screenCenter();
+	});
+	dodgeSystem.cameras = [camHUD];
+	dodgeSystem.add("reaction", function(bean:AttackSystem) {
+		FlxG.sound.play(Paths.sound("game/warning"));
+		FlxTween.tween(bean.warningLabel, {alpha: 1}, 0.075, {ease: FlxEase.circIn});
+	});
+	dodgeSystem.add("reaction_update", function(bean:AttackSystem, elapsed:Float) {
+		if(controlDodge.pressed)
+			bean.warningLabel.animation.play("press");
+		else bean.warningLabel.animation.play("static");
+	});
+	dodgeSystem.add("success", function(bean:AttackSystem) {
+		FlxTween.tween(bean.warningLabel, {alpha: 0}, 0.15);
+		
+		dadAttack(FlxPoint.get(dad.x, dad.y), "success");
+		attackSound.play();
+		charDodges(1, "success");
+	});
+	dodgeSystem.add("failed", function(bean:AttackSystem) {
+		FlxTween.tween(bean.warningLabel, {alpha: 0}, 0.15);
+		
+		dadAttack(FlxPoint.get(dad.x, dad.y), "failed");
+		attackNoDodgeSound.play();
+		charDodges(1, "failed");
+	});
+	add(dodgeSystem);
+}
+
+function stepHit(step:Int) {
+	switch(step) {
+		case 403 | 436 | 466 | 593 | 914 | 960 | 989 | 1040 | 1060 | 1152 | 1460 | 1508 | 1562 | 1650 | 1714 | 1778 | 1840 | 1919 | 1968:
+			execute();
+	}
+}
+
+var controllZoom = 0.65;
+function postUpdate(elapsed:Float) {
+	strumLines.forEachAlive(function(strumLine:StrumLine) {
+		if(characterLockCurAnim[strumLines.members.indexOf(strumLine)] == true) strumLine.characters[0].__lockAnimThisFrame = true;
+	});
+	
+	var controlCamera = characterLockCurAnim.contains(true);
+	if(controlCamera) {
+		camFollow.setPosition(850, 450);
+		camGame.zoom = lerp(FlxG.camera.zoom, controllZoom, 0.1);
+	}
+}
+
+public function execute() {
+	dodgeSystem.dispatch(0.5, function(timer:Float, rt:Float) {
+		if(cpuControlled) return true;
+
+		if(controlDodge.justPressed) return true;
+	});
+}
+
+function charDodges(i:Int, type:String) {
+	if(i == 0 || characterLockCurAnim[i] == true) return;
+	
+	var realType = type;
+	var char = strumLines.members[i].characters[0];
+	var recordv = FlxPoint.get(char.x, char.y);
+	characterLockCurAnim[i] = true;
+
+	if(type == "success") {
+		char.playAnim("dodge");
+		var finishTime = (1 / char.animation.curAnim.frameRate) * (char.animation.curAnim.numFrames);
+		new FlxTimer().start(finishTime, (tmr) -> characterLockCurAnim[i] = false);
+	}else {
+		characterLockCurAnim[i] = false;
+	}
+}
+
+function dadAttack(record:FlxPoint, type:String) {
+	if(characterLockCurAnim[0] == true) return;
+	
+	dad.playAnim("attack");
+
+	var realType = type;
+	var recordv = record;
+	var finishTime = (1 / dad.animation.curAnim.frameRate) * (dad.animation.curAnim.numFrames + 1);
+	FlxTween.tween(dad, {x: recordv.x + 550}, finishTime / 3, {ease: FlxEase.quadIn, startDelay: finishTime * 1/3, onComplete: (_) -> {
+		if(realType == "failed") {
+			camGame.flash(0xFFFF0000, 0.4);
+			
+			executeSendOut();
+			FlxTween.tween(dad, {x: recordv.x}, 0.25, {ease: FlxEase.quadOut, startDelay: finishTime * 1/5, onComplete: (_) -> {
+				characterLockCurAnim[0] = false;
+				recordv.put();
+			}});
+		}else {
+			FlxTween.tween(dad, {x: recordv.x}, 0.25, {ease: FlxEase.quadOut, startDelay: finishTime * 1/5, onComplete: (_) -> {
+				characterLockCurAnim[0] = false;
+				recordv.put();
+			}});
+		}
+	}});
+	characterLockCurAnim[0] = true;
+}
+
+function executeSendOut() {
+	drainCount -= (drainCount > 0 ? 1 : 0);
+}
