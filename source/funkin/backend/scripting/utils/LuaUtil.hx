8e90dccf9aaf299d1097484148826e176ce28885
GeXie <3449326235@qq.com>
2025-03-14 17:42:49 +0800

    Add files via upload
    Signed-off-by: GeXie <3449326235@qq.com>

--- source/funkin/backend/scripting/utils/LuaUtil.hx
+++ source/funkin/backend/scripting/utils/LuaUtil.hx
@@ -1,1 +1,14 @@
+package funkin.backend.scripting.utils;
 
+#if ALLOW_LUASTATE
+import funkin.backend.scripting.LuaScript;
+
+class LuaUtil {
+	public inline static final Function_Stop:Int = 0;
+	public inline static final Function_Continue:Int = 1;
+	
+	public static function reflectFunction(lua:LuaState) {
+		
+	}
+}
+#end
