3eb3fc6e4453e10275f340cbeee44a224af5718a
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2025-04-06 10:10:48 +0800

    Add files via upload
    Signed-off-by: tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>

--- mods/WBs Crisis/songs/Unprovoked/scripts/light and shadow.hx
+++ mods/WBs Crisis/songs/Unprovoked/scripts/light and shadow.hx
@@ -1,9 +1,30 @@
 function create() {
-	rtxLighting.innerShadowColor.set(0.8, 0.35, 0.35, 0.5);
-	rtxLighting.satinColor.set(0.65, 0.3, 0.2, 0.75);
-	rtxLighting.innerShadowAngle = (Math.PI / 180) * -45;
-	rtxLighting.innerShadowDistance = 35;
-	boyfriend.shader = rtxLighting.shader;
-	gf.shader = rtxLighting.shader;
-	dad.shader = rtxLighting.shader;
+    rtxLighting.innerShadowColor.set(0.5, 0.35, 0.5, 0.5);
+    rtxLighting.satinColor.set(0.5, 0.3, 0.2, 0.6);
+    rtxLighting.innerShadowAngle = (Math.PI / 180) * -45;
+    rtxLighting.innerShadowDistance = 35;
+
+    camHUD.addShader(rtxLighting.shader);
+
+    boyfriend.shader = rtxLighting.shader;
+    gf.shader = rtxLighting.shader;
+    dad.shader = rtxLighting.shader;
+}
+
+function stepHit(step:Int):Void {
+    switch (step) {
+        case 683:
+            FlxTween.tween(rtxLighting.innerShadowColor, {
+                r: 0.8, g: 0.35, b: 0.35, a: 0.5
+            }, 80);
+            
+            FlxTween.tween(rtxLighting.satinColor, {
+                r: 0.65, g: 0.3, b: 0.2, a: 0.6
+            }, 80);
+
+            FlxTween.tween(rtxLighting, {
+                innerShadowAngle: (Math.PI / 180) * -45,
+                innerShadowDistance: 35
+            }, 80);
+    }
 }
