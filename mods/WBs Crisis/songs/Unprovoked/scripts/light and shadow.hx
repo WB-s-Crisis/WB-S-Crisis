43e6d2f95963997fea8cabacf813bf5990e801b2
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2025-04-12 16:37:07 +0800

    Add files via upload
    Signed-off-by: tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>

--- mods/WBs Crisis/songs/Unprovoked/scripts/light and shadow.hx
+++ mods/WBs Crisis/songs/Unprovoked/scripts/light and shadow.hx
@@ -1,5 +1,5 @@
 function create() {
-    rtxLighting.innerShadowColor.set(0.5, 0.35, 0.5, 0.5);
+    rtxLighting.innerShadowColor.set(0.3, 0.6, 0.6, 0.5);
     rtxLighting.satinColor.set(0.5, 0.3, 0.2, 0.6);
     rtxLighting.innerShadowAngle = (Math.PI / 180) * -45;
     rtxLighting.innerShadowDistance = 35;
@@ -16,15 +16,15 @@ function stepHit(step:Int):Void {
         case 683:
             FlxTween.tween(rtxLighting.innerShadowColor, {
                 r: 0.8, g: 0.35, b: 0.35, a: 0.5
-            }, 80);
+            }, 65);
             
             FlxTween.tween(rtxLighting.satinColor, {
                 r: 0.65, g: 0.3, b: 0.2, a: 0.6
-            }, 80);
+            }, 65);
 
             FlxTween.tween(rtxLighting, {
                 innerShadowAngle: (Math.PI / 180) * -45,
                 innerShadowDistance: 35
-            }, 80);
+            }, 65);
     }
 }
